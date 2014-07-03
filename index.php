de32b78321e13592872c6c6de31ceaecbd1f857a
Cesar Zavala <cesarzavalamesta@g.harvard.edu>
2014-07-03 12:27:00 -0400

    Created a function & structured logic

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body class="<?php echo $class ?>">
-	<h1 class="<?php echo $class ?>">Beautiful</h1>
-	<img src="<?php echo $imagePath . $image ?>" alt="Background" >
+<body class="<?php echo dayPart() ?>">
+	<h1 class="<?php echo dayPart() ?>">Beautiful</h1>
+	<img src="<?php echo $image ?>" alt="Background" >
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,31 +1,29 @@
 
 <?php
-
 	error_reporting(E_ALL);
 
-	date_default_timezone_set('America/New_York');
-	$hour = (int)date('H');
-
 	$imagePath = "http://making-the-internet.s3.amazonaws.com/";
-	$imageMorning = "php-morning.png";
-	$imageAfternoon = "php-afternoon.png";
- 	$imageEvening = "php-evening.png";
-	$imageNight = "php-night.png"; 
+	$imagePrefix = "php-";
+	$imageSuffix = ".png";
 
 
+	function dayPart($timezone = 'America/New_York') {
+		date_default_timezone_set('America/New_York');
+		$hour = (int)date('H');
+
+		if (($hour>=5) and ($hour<11)) {
+			$class = "morning";
+		} elseif (($hour>=11) and ($hour<16)) {
+			$class = "afternoon";
+		} elseif (($hour>=16) and ($hour<20)) {
+			$class = "evening";
+		} else { 
+			$class = "night";
+		}
+		return $class;
+	} //function dayPart
+
+	$image = $imagePath . $imagePrefix . dayPart() . $imageSuffix;
 
 
-	if (($hour>=5) and ($hour<11)) {
-		$image = $imageMorning;
-		$class = "morning";
-	} elseif (($hour>=11) and ($hour<16)) {
-		$image = $imageAfternoon;
-		$class = "afternoon";
-	} elseif (($hour>=16) and ($hour<20)) {
-		$image = $imageEvening;
-		$class = "evening";
-	} else { 
-		$image = $imageNight; 
-		$class = "night";
-	}
 ?>
